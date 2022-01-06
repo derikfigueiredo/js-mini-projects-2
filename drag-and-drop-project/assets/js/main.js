a78ec2a80008733e0c20ff4b6ec3f73037895cc5
Dérik Figueiredo <derikdeewk@gmail.com>
2022-01-06 16:00:33 -0300

    making drag and drop work

--- drag-and-drop-project/assets/js/main.js
+++ drag-and-drop-project/assets/js/main.js
@@ -4,21 +4,27 @@ let dropzones = document.querySelectorAll('.dropzone')
 
 
 function dragStart() {
-  console.log('começou');
-  
+  dropzones.forEach( dropzone => dropzone.classList.add('highlight'));
+
+  this.classList.add('is-dragging');
 }
 
 function dragging() {
+  console.log('is dragging');
 }
 
 function dragEnd() {
+  dropzones.forEach( dropzone => dropzone.classList.remove('highlight'));
+
+  this.classList.remove('is-dragging');
+
 }
 
 
 cards.forEach((card) => {
   card.addEventListener('dragstart', dragStart);
   card.addEventListener('drag', dragging);
-  card.addEventListener('dragend', dragEnd)
+  card.addEventListener('dragend', dragEnd);
 })
 
 
@@ -34,11 +40,19 @@ function dragenter() {
 }
 
 function dragover() {
-  console.log('dragover')
+  this.classList.add('over');
+
+  
+  //get card being dragged
+  const cardBeingDragged = document.querySelector('.is-dragging');
+
+  //this = dropzone // putting dragging card on dropzone
+  this.appendChild(cardBeingDragged);
 }
 
 function dragleave() {
   console.log('saiu')
+  this.classList.remove('over');
 }
 
 function drop() {
